4a78ee09b0115b2f9684022e5a281adc60099a00
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2014-02-01 23:42:12 +0100

    Fix style and sndml.js, restore personal image

--- assets/js/sndml.js
+++ assets/js/sndml.js
@@ -1,5 +1,21 @@
-var links = document.getElementsByClassName('sndml'), l;
-// md5 of KILLSPAM
-for (l in links) {
-    links[l].href = links[l].href.replace(/377867bca0ab759b28b215d31e10ad8b/,'');
+var links;
+var className = 'sndml';
+
+if (document.getElementsByClassName) {
+    links = document.getElementsByClassName(className);
+} else {
+    var linksArray = [];
+    var aTags = document.getElementsByTagName('a');
+    var regex = new RegExp("(^|\\s)" + className + "(\\s|$)");
+    for (var i = 0; i < aTags.length; i++) {
+        if (regex.test(aTags[i].className)) {
+            linksArray.push(aTags[i]);
+        }
+    }
+    links = linksArray;
+}
+
+for (var i = 0; i < links.length; i++) {
+    // md5 of KILLSPAM
+    links[i].href = links[i].href.replace(/377867bca0ab759b28b215d31e10ad8b/,'');
 }
