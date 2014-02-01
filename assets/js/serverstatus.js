e198d9cfc7f97e405098d3f5fc6fec2b0ad8967f
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2014-02-01 15:43:16 +0100

    Update serverstatus.js

--- assets/js/serverstatus.js
+++ assets/js/serverstatus.js
@@ -1,14 +1,22 @@
-var url = 'http://pi.nicolalamacchia.com/status';
-var img = new Image();
-img.src = url;
+var url = 'http://pi.nicolalamacchia.com/status.php';
 var statusSpan = document.getElementById('pi-status');
 
-img.onload = function () {
-    statusSpan.innerHTML = 'online';
-    statusSpan.setAttribute('class', 'status-online');
+function UrlExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    try {
+        http.send();
+        s = http.status;
+        return (s >= 200 && s < 300 || s === 304);
+    } catch (ex) {
+        return false;
+    }
 }
 
-img.onerror = function () {
+if (UrlExists(url)) {
+    statusSpan.innerHTML = 'online';
+    statusSpan.setAttribute('class', 'status-online');
+} else {
     statusSpan.innerHTML = 'offline';
     statusSpan.setAttribute('class', 'status-offline');
 }
